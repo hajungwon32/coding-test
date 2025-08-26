de14f3d72239ab5daa7f52e122560bd1fff252d1
hajungwon32 <netrock49@gmail.com>
2025-08-26 09:30:42 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.03 ms, Memory: 87.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        String a = "";
+        String s = String.valueOf(n);
+        for(int i=0; i<s.length(); i++){
+            answer += s.charAt(i) - '0';
+        }
+        
+        return answer;
+    }
+}
